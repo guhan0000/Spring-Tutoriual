3b40168fb27d395e7deff4e26b31015e3e90954a
guhan0000 <guhanm0000@gmail.com>
2025-11-20 18:12:27 +0530

    Creation JDBC

--- src/main/java/org/database/Controller.java
+++ src/main/java/org/database/Controller.java
@@ -1,6 +1,7 @@
 package org.database;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,8 +9,23 @@ public class Controller {
 
 	public static void main(String[] args) throws SQLException {
 		
+		
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
 		Employees bean = context.getBean("employees",Employees.class);
+		
+//		Scanner scanner = context.getBean("scanner",Scanner.class);
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter EmpId");
+		int empId=scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("enter EmpName");
+		String empName=scanner.nextLine();
+		System.out.println("enter EmpAge");
+		int empAge=scanner.nextInt();
+		System.out.println("enter EmpDept");
+		String empDept=scanner.next();
+		bean.setEmpData(empId, empName, empAge, empDept);
 		bean.getEmpData();
 		
 
--- src/main/java/org/database/Employees.java
+++ src/main/java/org/database/Employees.java
@@ -2,6 +2,7 @@ package org.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,10 +12,27 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class Employees {
+	
+	
 	DataSource dataSource;
 //	DriverManagerDataSource
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		
+		
+	}
+		 void setEmpData(int empId,String empName,int empAge,String empDept) throws SQLException {
+			String sql="INSERT INTO employees (id,name,age,department) VALUES (?,?,?,?)";
+			Connection connection = dataSource.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, empId);
+			statement.setString(2, empName);
+			statement.setInt(3, empAge);
+			statement.setString(4, empDept);
+			int executeUpdate = statement.executeUpdate();
+		}
 	
-	
+		
 		void getEmpData() throws SQLException {
 			
 			String sql = "SELECT * FROM employees";
@@ -32,10 +50,6 @@ public class Employees {
 		}
 
 
-		public void setDataSource(DataSource dataSource) {
-			this.dataSource = dataSource;
-			
-			
-		}
+		
 
 }
