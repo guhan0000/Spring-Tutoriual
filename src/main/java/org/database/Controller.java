153229d1bfb30fca201b6cdfbb27744918b1985a
guhan0000 <guhanm0000@gmail.com>
2025-11-20 18:38:39 +0530

    Scanner Bean Created

--- src/main/java/org/database/Controller.java
+++ src/main/java/org/database/Controller.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Controller {
+	
+
+	
 
 	public static void main(String[] args) throws SQLException {
 		
@@ -13,18 +16,18 @@ public class Controller {
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
 		Employees bean = context.getBean("employees",Employees.class);
 		
-//		Scanner scanner = context.getBean("scanner",Scanner.class);
+		Scanner bean2 = context.getBean("scanner",Scanner.class);
 		
-		Scanner scanner=new Scanner(System.in);
+//		Scanner scanner=new Scanner(System.in);
 		System.out.println("enter EmpId");
-		int empId=scanner.nextInt();
-		scanner.nextLine();
+		int empId=bean2.nextInt();
+		bean2.nextLine();
 		System.out.println("enter EmpName");
-		String empName=scanner.nextLine();
+		String empName=bean2.nextLine();
 		System.out.println("enter EmpAge");
-		int empAge=scanner.nextInt();
+		int empAge=bean2.nextInt();
 		System.out.println("enter EmpDept");
-		String empDept=scanner.next();
+		String empDept=bean2.next();
 		bean.setEmpData(empId, empName, empAge, empDept);
 		bean.getEmpData();
 		
