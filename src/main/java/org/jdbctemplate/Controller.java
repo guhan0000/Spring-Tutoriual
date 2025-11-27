e1f1b81aa55425e363a36160317d7f5a623d97f8
guhan0000 <guhanm0000@gmail.com>
2025-11-27 13:25:42 +0530

    JdbcTEmplate CRUD performed

--- src/main/java/org/jdbctemplate/Controller.java
+++ src/main/java/org/jdbctemplate/Controller.java
@@ -8,16 +8,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Controller {
 	
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("jdbc_template.xml");
 		Student bean = context.getBean("student",Student.class);
-		bean.getStudentData();
-		
+//		bean.getStudentData();
+		Scanner scanner=new Scanner(System.in);
 		List<Map<String, Object>> studentData = bean.getStudentData();
-		for(Map<String, Object> student : studentData)
-		{
-			System.out.println(student);
-		}
+//		for(Map<String, Object> student : studentData)
+//		{
+//			System.out.println(student);
+//		}
+		System.out.println(studentData);
+//		System.out.println("enter sid");
+//		int sId=scanner.nextInt();
+//		System.out.println("enter email");
+//		String sEmail=scanner.next();
+//		System.out.println(bean.updateStudentEmail(sEmail,sId));
+//		scanner.nextLine();
+//		System.out.println("enter sName");
+//		String sName=scanner.nextLine();
+//		
+//		System.out.println("enter age");
+//		int sAge=scanner.nextInt();
+//		System.out.println(bean.insertStudentData(sId, sName, sAge));
+		
 		
 	}
 
--- src/main/java/org/jdbctemplate/Student.java
+++ src/main/java/org/jdbctemplate/Student.java
@@ -15,11 +15,52 @@ public class Student {
 	DataSource dataSource;
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+	private int update;
 	
-	public List<Map<String,Object>> getStudentData() throws SQLException{
+	public List<Map<String,Object>> getStudentData(){
 		return jdbcTemplate.queryForList("SELECT * FROM students");
 		
 	}
+	 String insertStudentData(int sId,String sName,int sAge) {
+		Object arr[]= {sId,sName,sAge};
+		String sql="INSERT INTO students (student_id,name,age) VALUES (?,?,?)";
+		update = jdbcTemplate.update(sql,arr);
+		 if(update==1)
+		 {
+			 return "Student Inserted Successfully";
+		 }
+		 else {
+			return "Student not inserted";
+		}
+		
+
+	}
+	 String deleteStudent(int sId) {
+		 Object arr[]= {sId};
+		 int delete = jdbcTemplate.update("DELETE FROM students WHERE student_id=?",arr);
+		 if(delete==1)
+		 {
+			 return "Student deleted";
+		 }
+		 else {
+			 return "Student not deleted";
+		 }
+		 
+	 }
+	 String updateStudentEmail(String sEmail,int sId)
+	 {
+		 Object arr[]= {sEmail,sId};
+		 int emailUpdate = jdbcTemplate.update("UPDATE students SET email=? WHERE student_id=?",arr);
+		 if(emailUpdate==1)
+		 {
+			 return "Email Updated";
+		 }
+		 else {
+			return "Email not Updated";
+		}
+	 
+	 }
+	  
 	
 
 }
